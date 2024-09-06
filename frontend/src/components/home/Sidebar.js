4d1251dd9e7ebbdb1676a9c7e2b112757297cbe2
Spfeed <subbart2266@gmail.com>
2024-09-07 03:39:23 +0400

    Добавлена фильтрация.

--- frontend/src/components/home/Sidebar.js
+++ frontend/src/components/home/Sidebar.js
@@ -3,16 +3,67 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import styles from "./Sidebar.module.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const Sidebar = (props) => {
+const Sidebar = ({ onSearchByTitle, onSearchByActor, onGenreChange }) => {
+  const [titleKeyword, setTitleKeyword] = useState("");
+  const [actorKeyword, setActorKeyword] = useState("");
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+
+  useEffect(() => {
+    const fetchGenres = async () => {
+      try {
+        const response = await axios.get("/genres");
+        setGenres(response.data);
+      } catch (error) {
+        console.error("Ошибка при получении жанров: ", error);
+      }
+    };
+
+    fetchGenres();
+  }, []);
+
+  const handleGenreChange = (genreName) => {
+    setSelectedGenres((prevSelectedGenres) =>
+      prevSelectedGenres.includes(genreName)
+        ? prevSelectedGenres.filter((g) => g !== genreName)
+        : [...prevSelectedGenres, genreName]
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onGenreChange(selectedGenres);
+  };
+
+  const handleSearchByTitle = (e) => {
+    e.preventDefault();
+    if (titleKeyword.trim()) {
+      onSearchByTitle(titleKeyword.trim());
+    }
+  };
+
+  const handleSearchByActor = (e) => {
+    e.preventDefault();
+    if (actorKeyword.trim()) {
+      onSearchByActor(actorKeyword.trim());
+    }
+  };
   return (
     <div className={styles.sidebar}>
       <h2>Фильрация</h2>
       <Container className={styles.container}>
-        <Form className={styles.form}>
+        <Form className={styles.form} onSubmit={handleSearchByTitle}>
           <Form.Group controlId="searchByTitle">
             <Form.Label>Поиск по названию</Form.Label>
-            <Form.Control type="text" placeholder="Введите название" />
+            <Form.Control
+              type="text"
+              placeholder="Введите название"
+              value={titleKeyword}
+              onChange={(e) => setTitleKeyword(e.target.value)}
+            />
           </Form.Group>
           <Button
             variant="primary"
@@ -24,12 +75,40 @@ const Sidebar = (props) => {
         </Form>
       </Container>
       <Container className={styles.container}>
-        <Form>
+        <Form onSubmit={handleSearchByActor}>
           <Form.Group controlId="searchByName">
             <Form.Label>Поиск по имени и фамилии актера</Form.Label>
-            <Form.Control type="text" placeholder="Введите имя и фамилию" />
+            <Form.Control
+              type="text"
+              placeholder="Введите имя и фамилию (фамилию или имя)"
+              value={actorKeyword}
+              onChange={(e) => setActorKeyword(e.target.value)}
+            />
           </Form.Group>
 
+          <Button
+            variant="primary"
+            type="submit"
+            className={styles.searchbutton}
+          >
+            Поиск
+          </Button>
+        </Form>
+      </Container>
+      <Container className={styles.container}>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="searchByGenres">
+            <Form.Label>Фильтр по жанрам</Form.Label>
+            {genres.map((genre) => (
+              <Form.Check
+                key={genre.name}
+                type="checkbox"
+                label={genre.name}
+                checked={selectedGenres.includes(genre.name)}
+                onChange={() => handleGenreChange(genre.name)}
+              />
+            ))}
+          </Form.Group>
           <Button
             variant="primary"
             type="submit"
--- frontend/src/components/home/SidebarAndCards.js
+++ frontend/src/components/home/SidebarAndCards.js
@@ -8,52 +8,106 @@ import MovieCreateModal from "./MovieCreateModal";
 const SidebarAndCards = (props) => {
   const [movies, setMovies] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const [filterKeyWord, setFilterKeyWord] = useState("");
+  const [filterType, setFilterType] = useState("");
+  const [emptyResult, setEmptyResult] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (url, method = "GET", data = null) => {
     try {
-      const response = await axios.get("/movies/details");
-      setMovies(response.data);
+      setEmptyResult(false);
+      const config = {
+        method,
+        url,
+        data,
+      };
+      const response = await axios(config);
+      if (response.status === 204) {
+        setMovies([]);
+        setEmptyResult(true);
+      } else {
+        setMovies(response.data);
+      }
     } catch (error) {
       console.error("Ошибка при получении данных: ", error);
     }
   };
+
   useEffect(() => {
-    fetchMovies();
-  });
+    if (!filterKeyWord) {
+      fetchMovies("/movies/details");
+    }
+  }, [filterKeyWord]);
 
   const handleShowModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
 
   const refreshMovies = () => {
-    fetchMovies();
+    if (filterType === "title") {
+      fetchMovies(`/movies/serviceFiltration/${filterKeyWord}`);
+    } else if (filterType === "actor") {
+      fetchMovies(`/movies/filterByActor/${filterKeyWord}`);
+    } else if (filterType === "genre") {
+      fetchMovies("/movies/filterByGenres", "POST", filterKeyWord);
+    } else {
+      fetchMovies("/movies/details");
+    }
+  };
+
+  const handleSearchByTitle = (keyword) => {
+    setFilterKeyWord(keyword);
+    setFilterType("title");
+    fetchMovies(`/movies/serviceFiltration/${keyword}`);
+  };
+
+  const handleSearchByActor = (keyword) => {
+    setFilterKeyWord(keyword);
+    setFilterType("actor");
+    fetchMovies(`/movies/filterByActor/${keyword}`);
+  };
+
+  const handleGenreChange = (selectedGenres) => {
+    setFilterKeyWord(selectedGenres);
+    setFilterType("genre");
+    fetchMovies("/movies/filterByGenres", "POST", selectedGenres);
   };
 
   return (
     <>
       <div className={styles.container}>
-        <Sidebar />
-        <div className={styles.cards}>
-          <h1 className={styles.cardsTitle}>Фильмы</h1>
-          <div className={styles.createButtonContainer}>
-            <button className={styles.createButton} onClick={handleShowModal}>
-              Создать
-            </button>
+        <Sidebar
+          onSearchByTitle={handleSearchByTitle}
+          onSearchByActor={handleSearchByActor}
+          onGenreChange={handleGenreChange}
+        />
+        {!emptyResult ? (
+          <div className={styles.cards}>
+            <h1 className={styles.cardsTitle}>Фильмы</h1>
+            <div className={styles.createButtonContainer}>
+              <button className={styles.createButton} onClick={handleShowModal}>
+                Создать
+              </button>
+            </div>
+            <div className={styles.cardsContainer}>
+              {movies.map((movie, index) => (
+                <MovieCard
+                  key={index}
+                  title={movie.title}
+                  releaseYear={movie.releaseYear}
+                  description={movie.description}
+                  genre={movie.genre}
+                  actors={movie.actors}
+                  rating={movie.rating}
+                  refreshMovies={refreshMovies}
+                />
+              ))}
+            </div>
           </div>
-          <div className={styles.cardsContainer}>
-            {movies.map((movie, index) => (
-              <MovieCard
-                key={index}
-                title={movie.title}
-                releaseYear={movie.releaseYear}
-                description={movie.description}
-                genre={movie.genre}
-                actors={movie.actors}
-                rating={movie.rating}
-                refreshMovies={refreshMovies}
-              />
-            ))}
-          </div>
-        </div>
+        ) : (
+          <p className={styles.emptyResult}>
+            Фильмы по вашему запросу не найдены, попробуйте ввести другой запрос
+            или перезагрузите страницу.
+          </p>
+        )}
       </div>
       <MovieCreateModal
         show={showModal}
